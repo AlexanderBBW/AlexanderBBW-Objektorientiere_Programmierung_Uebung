7c9452716d385581a446f8c34245c37b4cc78b92
AlexanderBBW <49153828+AlexanderBBW@users.noreply.github.com>
2020-04-14 15:49:13 +0200

    Add files via upload

---
+++ Fahrzeug/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fahrzeug
+{
+    public class Identifizierung
+    {
+        public string Baujahr { get; set; }
+        public string Marke { get; set; }
+        public string Modell { get; set; }
+        public string Laufleistung { get; set; }
+        public string Kennzeichen { get; set; }
+        public string Eigentümer { get; set; }
+
+        public Identifizierung(string Modell, string Eigentümer, string Kennzeichen)
+        {
+            this.Modell = Modell;
+            this.Eigentümer = Eigentümer;
+            this.Kennzeichen = Kennzeichen;
+        }
+        public Identifizierung(string Modell, string Eigentümer, string Kennzeichen, string Baujahr)
+        {
+            this.Modell = Modell;
+            this.Eigentümer = Eigentümer;
+            this.Kennzeichen = Kennzeichen;
+            this.Baujahr = Baujahr;
+        }
+        public string erhoeheLaufleistung(string Laufleistung)
+        {
+            return ("Die Laufleistung beträgt:" + Laufleistung +"Kilometer");
+        }
+    }
+}
