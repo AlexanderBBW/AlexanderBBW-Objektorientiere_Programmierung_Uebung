8ebac502a2cc5a34de4c5e2fd15cc023d9cd45f9
AlexanderBBW <49153828+AlexanderBBW@users.noreply.github.com>
2020-04-16 16:01:46 +0200

    Add files via upload

--- Azubis/Program.cs
+++ Azubis/Program.cs
@@ -16,24 +16,61 @@ namespace Azubis
                 this.Vorname = Vorname;
                 this.Geburtsdatum = Geburtsdatum;
             }
-            private void berechneAlter(DateTime Geburtsdatum)
+            public int berechneAlter()
             {
-                int Jahr = DateTime.Now.Year - Geburtsdatum.Year;
-                if (DateTime.Now.CompareTo(Geburtsdatum) < 0) { Jahr--; }
-                gibSteckbriefAus(Jahr);
+                int alter = DateTime.Now.Year - Geburtsdatum.Year;
+                if (DateTime.Now.CompareTo(Geburtsdatum) < 0) { alter--; }
+                 return (alter);
             }
-            public void gibSteckbriefAus(int Jahr)
+            public void gibSteckbriefAus()
             {
-                Console.WriteLine("Name:" + " " + Nachname + " " + "Vorname: " + " " + Vorname + " " + "Alter:" + " " + Jahr);
-                 Console.WriteLine(Jahr);
+                Console.WriteLine("Name:" + " " + Nachname + " " + "Vorname: " + " " + Vorname + " " + "Alter:" + " " + berechneAlter());
             }
         }
         class Program
         {
             static void Main(string[] args)
             {
-                Azubi MeinAzubi = new Azubi("Grunert", "Alexander", Convert.ToDateTime("09.02.2001"));
-                Console.ReadKey();
+            AuswahlMenue();
+            }
+        static void AuswahlMenue()
+        {
+            Console.WriteLine("Bitte geben Sie '1' an um einen neuen Azubi anzulegen oder '0' um sich die Azubis anzeigen zu lassen!");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    AzubiEingabe();
+                    break;
+                case "0":
+                    AzubiAusgabe();
+                    break;
+                default:
+                    Console.WriteLine("Dies war keine gültige Eingabe, drücken sie eine beliebige Taste um zur erneuten Auswahl zu kommen!");
+                    Console.ReadKey();
+                    AuswahlMenue();
+                break;
             }
         }
+        static object AzubiEingabe()
+        {
+            Console.WriteLine("Bitte geben Sie den Nachnamen an!");
+            string Nachname = Console.ReadLine();
+            Console.WriteLine("Bitte geben Sie den Vornamen an!");
+            string Vorname = Console.ReadLine();
+            Console.WriteLine("Bitte geben Sie den Nachnamen an!");
+            DateTime GeburtsDatum = Convert.ToDateTime(Console.ReadLine());
+            Azubi MeinAzubi = new Azubi(Nachname, Vorname, GeburtsDatum);
+            AzubiAnzeigen(MeinAzubi);
+            return (MeinAzubi);
+        }
+        static void AzubiAnzeigen(object MeinAzubi)
+        {
+            Console.WriteLine(MeinAzubi);
+            Console.ReadLine();
+        }
+        static void AzubiAusgabe()
+        {
+
+        }
     }
+}
